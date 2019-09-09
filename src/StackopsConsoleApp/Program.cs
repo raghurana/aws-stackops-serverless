ed9613f032e216b36e98b1e1f4ee82fd606c64df
Raghu Rana <raghurana@gmail.com>
2019-09-09 12:34:35 +1000

    Initial skeleton commit

---
+++ src/StackopsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackopsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/StackopsCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StackopsCore
+{
+    public class Class1
+    {
+    }
+}
