417fe0fd769e92f02fc50bf3d285fa1d4d461d22
Raghu Rana <raghurana@gmail.com>
2019-09-12 10:09:33 +1000

    Added exception handling code for the Lambda function

--- src/StackopsServerlessFunctions/Function.cs
+++ src/StackopsServerlessFunctions/Function.cs
@@ -5,7 +5,6 @@ using Autofac;
 using MediatR;
 using StackopsCore;
 using StackopsCore.Factories;
-using StackopsCore.Models;
 using StackopsCore.Utils;
 using System;
 using System.Threading.Tasks;
@@ -22,19 +21,44 @@ namespace StackopsServerlessFunctions
             {
                 using(var bootstrap = new LambdaBootstrap(handlerWrapper))
                 {
-                    await bootstrap.RunAsync();
+                    try
+                    {
+                        await bootstrap.RunAsync();
+                    }
+
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("An exception occured with message below:");
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine(ex.Message);
+                    
+                        Console.WriteLine("Stack Trace:");
+                        Console.WriteLine("------------");
+                        Console.WriteLine(ex.StackTrace);
+
+                        if(ex.InnerException != null)
+                        {
+                            Console.WriteLine("Inner Exception:");
+                            Console.WriteLine("---------------");
+                            Console.WriteLine(ex.InnerException.Message);
+                        }
+                    }
                 }
             }
         }
 
         public static string FunctionHandler(string actionRequest, ILambdaContext context)
         { 
+            Console.WriteLine($"Received Action: {actionRequest}");
+
             using(var scope = DependencyInjection.Init())
             {
                 var configJsonPath  = FileUtils.GetAbsolutePathFromCurrentDirectory("stacks.json");
                 var allStacks       = StackFactory.CreateStacksFromJson(configJsonPath);
                 var mediatorRequest = StackRequestFactory.CreateMediatorRequest(allStacks, actionRequest);
 
+                Console.WriteLine("Stacks affected:");
+                Array.ForEach( allStacks, stack => Console.WriteLine(stack.Name));
                 scope.Resolve<IMediator>().Send(mediatorRequest).Wait();    
             }
 
